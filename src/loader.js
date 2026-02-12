aeb656cd7373c4f26458e6753c6516d6b3b5e97a
Chen-Heng CHANG <wandererm@gmail.com>
2026-02-12 22:38:06 +0800

    Replace PxLoader with native image preload

--- src/loader.js
+++ src/loader.js
@@ -1,21 +1,27 @@
-import { PxLoader } from "./pxloader.js";
 import { FILES } from "./constants.js";
 
 export function initLoader(onComplete) {
-  var loader = new PxLoader();
-  Object.keys(FILES).forEach(function(key) {
-    loader.addImage(FILES[key]);
+  var urls = Object.keys(FILES).map(function(key) {
+    return FILES[key];
   });
-  loader.addProgressListener(function(e) {
-    if (e.completedCount * 6 < 100) {
-      document.getElementById("loading").querySelector("span").textContent =
-        e.completedCount * 6 + "%";
+  var loadingText = document.getElementById("loading").querySelector("span");
+  var completedCount = 0;
+  var totalCount = urls.length;
+
+  function onImageDone() {
+    completedCount += 1;
+    var pct = Math.floor((completedCount / totalCount) * 100);
+    loadingText.textContent = pct + "%";
+    if (completedCount >= totalCount) {
+      document.getElementById("loading").classList.add("hidden");
+      onComplete();
     }
+  }
+
+  urls.forEach(function(url) {
+    var img = new Image();
+    img.onload = onImageDone;
+    img.onerror = onImageDone;
+    img.src = url;
   });
-  loader.addCompletionListener(function() {
-    document.getElementById("loading").classList.add("hidden");
-    onComplete();
-  });
-  loader.start();
-  return loader;
 }
